950a13cc9c504d8fd916f27c256065d3e45c0da1
Antonio Insuasti <antonio@insuasti.ec>
2017-10-14 01:34:38 -0500

    Change deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 if [ $TRAVIS_BRANCH == 'test' ]
 then
-  mv coverage/Firefox\ 55.0.0\ \(Fedora\ 0.0.0\) coverage/report
+  cd coverage
+  mv Firefox\ 55.0.0\ \(Fedora\ 0.0.0\) report
+  cd ..
 else
   rm -rf coverage
 fi
